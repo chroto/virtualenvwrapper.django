c56af751f427bdb58cd5757a7c1b871c98555122
Chris Proto <chroto24+github@gmail.com>
2012-12-30 10:24:49 -0700

    Fallback to latest Django correctly

--- virtualenvwrapper/django_project.py
+++ virtualenvwrapper/django_project.py
@@ -12,21 +12,22 @@ log = logging.getLogger('virtualenvwrapper.django')
 
 def template(args):
     project = args[0]
-    template_arg = ''
     DJANGO_TEMPLATE = os.getenv('DJANGO_TEMPLATE', '')
-    if DJANGO_TEMPLATE != '':
-        requirements_file = os.path.join(DJANGO_TEMPLATE, 'requirements.txt')
-        subprocess.check_call([
-            'pip',
-            'install',
-            '-r',
-            requirements_file
-        ])
-        template_arg = '--template=%s' % DJANGO_TEMPLATE
-
-    subprocess.check_call([
+    requirements = os.path.join(DJANGO_TEMPLATE, 'requirements.txt')
+    pip_args = [
+        'pip',
+        'install'
+    ]
+    if os.path.exists(requirements):
+        pip_args.extend(['-r', requirements])
+    else:
+        pip_args.append('Django')
+    subprocess.check_call(pip_args)
+    start_project_args = [
         'django-admin.py',
         'startproject',
-        project,
-        template_arg
-    ])
+        project
+    ]
+    if DJANGO_TEMPLATE != '':
+        start_project_args.append('--template=%s' % DJANGO_TEMPLATE)
+    subprocess.check_call(args)
